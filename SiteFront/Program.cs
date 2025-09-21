c450e97057677d20cd2ebbfa34ac26769f944948
Mohamed abdelaziz <Mohamednosser12345@gmail.com>
2025-09-21 22:31:25 +0300

    Configure Kestrel and update WhatsApp API settings
    - Modified `Program.cs` to configure Kestrel to listen on all IPs at port 5000 in production.
    - Added `EndTimeCheckerService` as a hosted service.
    - Updated WhatsApp API `BaseUrl` and `Token` in `appsettings.json`.
    - Added a new binary file `8c1761ea-b2f8-4929-928b-b784bbadb918.jpg`.

--- SiteFront/Program.cs
+++ SiteFront/Program.cs
@@ -57,6 +57,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.Listen(IPAddress.Any, 5000);
+    });
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
